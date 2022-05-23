5d65516969aaf28e4858588cef4766af6544bc39
d-vinet <85059122+d-vinet@users.noreply.github.com>
2022-05-23 20:55:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 if [[ -n $RCLONE_CONFIG_BASE_URL ]]; then
 	echo "Rclone config detected"
-     mkdir /app/.config && mkdir /app/.config/rclone && wget -O /app/.config/rclone/rclone.conf $RCLONE_CONFIG_BASE_URL
+     mkdir /root/.config && mkdir /root/.config/rclone && wget -O /root/.config/rclone/rclone.conf $RCLONE_CONFIG_BASE_URL
 fi
 
 if [[ -n $BOT_TOKEN && -n $OWNER_ID ]]; then
